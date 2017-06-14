46a2d54c8bc29c102aadd35c218cae9c101350ec
Arjun Sripathy <arjun.sripathy2000@gmail.com>
2017-06-13 22:25:43 -0700

    data test

--- abalone.py
+++ abalone.py
@@ -7,6 +7,8 @@ datafile = open("abalonedata.txt","r")
 for line in datafile:
 	data.append(line)
 
+DATA_SPLIT = 3500
+
 maleAtt = []
 femAtt = []
 infAtt = []
@@ -15,7 +17,7 @@ maleAge = []
 femAge = []
 infAge = []
 
-for d in data[:10]:
+for d in data[0:DATA_SPLIT]:
 	x = d.split(",")
 	atts = x[1:len(x)-1]
 	for i in range(len(atts)):
@@ -30,4 +32,31 @@ for d in data[:10]:
 	if(x[0]=="I"):
 		infAtt.append(atts)
 		infAge.append(age)
+
+tmaleAtt = []
+tfemAtt = []
+tinfAtt = []
+
+tmaleAge = []
+tfemAge = []
+tinfAge = []
+
+for d in data[DATA_SPLIT:]:
+	x = d.split(",")
+	atts = x[1:len(x)-1]
+	for i in range(len(atts)):
+		atts[i]=float(atts[i])
+	age = int(x[len(x)-1].strip())
+	if(x[0]=="M"):
+		tmaleAtt.append(atts)
+		tmaleAge.append(age)
+	if(x[0]=="F"):
+		tfemAtt.append(atts)
+		tfemAge.append(age)
+	if(x[0]=="I"):
+		tinfAtt.append(atts)
+		tinfAge.append(age)
+
+
+
 
